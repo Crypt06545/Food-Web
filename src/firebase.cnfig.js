63655c6a009388d04b638eaeb36e205a7ae10c3a
Crypt06545 <mehedihasansagor2005@gmail.com>
2023-01-15 10:49:37 +0600

    first commit

---
+++ src/firebase.cnfig.js
@@ -0,0 +1,16 @@
+// import { initializeApp } from "firebase/app";
+// import {getAuth} from 'firebase/auth'
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBPfpl4Mh_QGYJATYlnKIrRb8IJ_IjqY1Y",
+//   authDomain: "food-web-d00b5.firebaseapp.com",
+//   projectId: "food-web-d00b5",
+//   storageBucket: "food-web-d00b5.appspot.com",
+//   messagingSenderId: "519142110351",
+//   appId: "1:519142110351:web:e472f536593f80c9276cfa"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export default app;
